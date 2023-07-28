029a66a56341c8028d415436faaa974012b899a2
Alessandro Fernandes <alessandro_h15@hotmail.com>
2023-07-28 12:32:10 -0300

    Update
    Resolucao Javascript

--- Mod2Proj9.js
+++ Mod2Proj9.js
@@ -2,3 +2,35 @@
 
 Caso o usuário não digite um número ou apareça um inválido no campo do ano, o sistema informará o erro e continuará perguntando até que um valor correto seja preenchido.*/
 
+function entrarAnoValido() {
+    
+    while (true) {
+    
+    var anoNascimento = prompt("Digite o ano de nascimento (entre 1922 e 2023): ")
+    var ano = parseInt(anoNascimento)
+
+    if (!isNaN(ano) && ano >= 1922 && ano <= 2023) {
+    
+        return ano
+
+    } else {
+        
+        alert("Tente novamente, o ano deve estar entre 1922 e 2023.")
+
+        }
+    }
+}
+
+function calcularIdade() {
+    
+    var nome = prompt("Digite seu nome completo: ")
+    var anoDeNascimento = entrarAnoValido()
+    var anoAtual = new Date().getFullYear()
+    var idade = anoAtual - anoDeNascimento
+
+
+    alert('Olá, ' + nome + '! Você completou ou completará ' + idade + ' anos em ' + anoAtual + '.')
+}
+
+calcularIdade()
+
