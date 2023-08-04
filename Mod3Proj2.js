25cf4ba44536f0de71e872b76d0a5196dce56b10
Alessandro Fernandes <alessandro_h15@hotmail.com>
2023-08-04 18:25:42 -0300

    Mod3Proj2

---
+++ Mod3Proj2.js
@@ -0,0 +1,15 @@
+/* Modulo 3 - Projeto 2
+
+Nesta atividade, você deve criar um código em JavaScript com uma matriz bidimensional para armazenar informações sobre animais e as suas características.
+
+Cada linha deve representar um animal e cada coluna terá as diferentes informações sobre ele, como nome, espécie e idade.*/
+
+let animais = [
+    ['Leão', 'Mamifero', 18],
+    ['Cachorro', 'Mamifero', 10],
+    ['Besouro', 'Inseto', 1],
+    ['Falcão','Ave', 3],
+    ['Lagarto', 'Reptil', 3]
+]
+
+console.log(animais)
