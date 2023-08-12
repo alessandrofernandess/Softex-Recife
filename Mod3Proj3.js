50ce1da0067bada6154b7611ec33f051d7f6da43
Alessandro Fernandes <alessandro_h15@hotmail.com>
2023-08-12 20:14:46 -0300

    upDate

--- Mod3Proj3.js
+++ Mod3Proj3.js
@@ -4,7 +4,8 @@ Considere o array [3, 7, 9, 1, 0].
 
 O primeiro elemento inserido foi o 3, o segundo foi o 7 e assim por diante, seguindo a ordem.
 
-Levando em conta as três estruturas de dados (fila, lista e pilha) e as suas regras de operações, qual seria a sequência dos números removidos em cada uma delas para esvaziá-las completamente utilizando o array mencionado?
+Levando em conta as três estruturas de dados (fila, lista e pilha) e as suas regras de operações,
+qual seria a sequência dos números removidos em cada uma delas para esvaziá-las completamente utilizando o array mencionado?
 
 */
 
