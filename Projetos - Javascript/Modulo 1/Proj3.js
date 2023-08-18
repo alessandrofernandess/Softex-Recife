eb028b1ed3ed36096b7348f2136ea0902f42c745
Alessandro Fernandes <alessandro_h15@hotmail.com>
2023-08-18 12:59:39 -0300

    upDate

---
+++ Projetos - Javascript/Modulo 1/Proj3.js
@@ -0,0 +1,69 @@
+// Modulo 1 - Projeto 3
+
+// Exibindo Undefined. 
+
+console.log('----- Undefined -----')
+
+console.log('- Boolean -')
+    let booleanUndefined
+        console.log(booleanUndefined)    
+
+console.log('- BigInt -')
+    let bigIntUndefined
+        console.log(bigIntUndefined)
+
+console.log('- String -')
+    let stringUndefined
+        console.log(stringUndefined)
+
+console.log('- Symbol -')
+    let symbolUndefined
+        console.log(symbolUndefined)
+
+// Exibindo Valores Nulos
+
+console.log('----- Null ------')
+
+console.log('- Boolean -')
+    let booleanNull = null
+        console.log(booleanNull)
+
+console.log('- BigInt -')
+    let bigIntNull = null
+        console.log(bigIntNull)
+
+console.log('- String -')
+    let stringNull = null
+        console.log(stringNull)
+
+console.log('- Symbol -')
+    let symbolNull = null
+        console.log(symbolNull)
+
+// Exibindo Valores Atribuidos
+
+console.log('----- Atribuições -----')
+
+console.log('- Boolean -')
+
+    let booleano = true
+        if (booleano){
+            console.log('Verdadeiro')
+        } else{
+            console.log('Falso')
+        }
+
+console.log('- BigInt -')
+    
+    let bigInt = 123456789123456789n
+        console.log(bigInt)
+
+console.log('- String -')
+    
+    let string = 'Alessandro'
+        console.log(string)
+
+console.log('- Symbol -')
+
+    let simbolo = Symbol('Exemplo')
+        console.log(simbolo)
