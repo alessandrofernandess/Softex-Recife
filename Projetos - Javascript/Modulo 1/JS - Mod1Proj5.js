b531fda817d6d78837d19e9beea4741314c4104d
Alessandro Fernandes <alessandro_h15@hotmail.com>
2023-08-19 18:28:14 -0300

    upDate

---
+++ Projetos - Javascript/Modulo 1/JS - Mod1Proj5.js
@@ -0,0 +1,10 @@
+// Modulo 1 - Projeto 5
+
+// Calculadora de Médias
+
+var nota1 = parseFloat(prompt('Insira a sua primeira nota: '))
+var nota2 = parseFloat(prompt('Insira a sua segunda nota: '))
+var nota3 = parseFloat(prompt('Insira a sua terceira nota: '))
+var media = (nota1 + nota2 + nota3)/3
+
+console.log('Sua média é: ' + media)
