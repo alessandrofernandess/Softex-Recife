b57b281b994a89b0a67c0cb7224476ff6d80a362
Alessandro Fernandes <alessandro_h15@hotmail.com>
2023-08-25 11:54:15 -0300

    upDate

---
+++ Projetos - Javascript/Modulo 2/JS - Mod2Proj2.js
@@ -0,0 +1,32 @@
+// Modulo 2 - Projeto 2
+
+// Calculadora
+
+let num1 = parseFloat(prompt('Vamos calcular dois numeros, digite o primeiro: '))
+let operacao = prompt('Digite a operação desejada: ')
+let num2 = parseFloat(prompt('Digite o outro numero: '))
+
+let resultado
+
+if(operacao == '+'){                                                      //Soma
+    resultado = num1 + num2
+        console.log(`${num1} + ${num2} = ${resultado}`)
+}
+
+if(operacao == '-'){                                                      //Subtração
+    resultado = num1 - num2
+        console.log(`${num1} - ${num2} = ${resultado}`)
+}
+
+if(operacao == '*'){                                                      //Multiplicação
+    resultado = num1 * num2
+        console.log(`${num1} x ${num2} = ${resultado}`)
+}
+
+if(operacao == '/'){                                                      //Divisão
+    if (num2 !== 0) {
+        resultado = num1 / num2  
+            let resto = num1 % num2
+                console.log(`${num1} / ${num2} = ${resultado} Resto: ${resto}`)
+    }
+}
