b1decc43ae2142160d87eb0958726edcdd798910
[alessandrofernandess] <[alessandro_h15@hotmail.com]>
2023-11-17 11:44:32 -0300

    upDate
    Correção

--- Projetos - Padroes de Desenvolvimento de Software/Modulo 1/Mod1Proj1.js
+++ Projetos - Padroes de Desenvolvimento de Software/Modulo 1/Mod1Proj1.js
@@ -1,5 +1,19 @@
 // Modulo 1 - Projeto 1
 
+/*
+
+Considere que:
+
+- Os produtos devem implementar uma interface comum.
+- O cliente deve utilizar apenas a fábrica para criar instâncias do tipo abstrato da interface.
+- Todo computador deve ter, com getters, os atributos: ram, hdd, cpu e type há dois tipos de computadores: pc e server.
+- RAM e HD devem estar em GB.
+- CPU deve estar em GHz.
+- Através do tipo informado, a fábrica decide qual tipo de computador irá instanciar.
+- Quando o método .toString() for aplicado a um computador, ele deve imprimir seus atributos. */
+
+// Resolução
+
 // Interface comum para os produtos
 
 class Computer {
